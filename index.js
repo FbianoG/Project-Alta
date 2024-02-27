eaeea5e4a34591837bbdec37a6677dd64b751d41
F.Gonçalves <boriscasoy123@gmail.com>
2024-02-27 09:23:46 -0300

    f

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 const express = require('express')
 const router = require('./src/routes/router')
-const DataBase = require('./src/dataBase/db')
+const DB = require('./src/dataBase/db')
 
-const cors = require('cors');
+const cors = require('cors')
 const app = express()
 const port = 3000
 
-app.use(cors());
+
+const corsOptions = {
+    origin: 'https://fbianog.github.io/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    exposedHeaders: 'Authorization',
+}
+
+app.use(cors(corsOptions)) // Configurar CORS antes das rotas
+
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
 
-DataBase.ConnectDataBase()
-
 app.listen(port, () => {
     console.log(`Servidor funcionando na porta:`, port)
+    DB.ConnectDataBase()
 })
-
-
-
-
-
-
-
-
-
-
