71cbfa9b08316584ccd3b4d0c1e501904541c2d0
F.Gonçalves <boriscasoy123@gmail.com>
2024-02-27 10:57:59 -0300

    s

--- src/dataBase/db.js
+++ src/dataBase/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 async function ConnectDataBase() {
     try {
-       const connectedDB =  await mongoose.connect(`mongodb+srv://alta:alta123@cluster0.s2g9xui.mongodb.net/?retryWrites=true&w=majority`)
+       const connectedDB =  await mongoose.connect(`mongodb+srv://alta:alta@cluster0.gzsimsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
         if (connectedDB) {
             console.log("DataBase connected!")
         }
