bfbd0d8f0f09a6a9910c18781d66c01d453d3308
F.Gonçalves <boriscasoy123@gmail.com>
2024-02-27 09:31:10 -0300

    d

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express')
+const cors = require('cors')
 const router = require('./src/routes/router')
 const DB = require('./src/dataBase/db')
-
-const cors = require('cors')
-const app = express()
-const port = 3000
-
 
 const corsOptions = {
     origin: '*',
@@ -16,6 +12,11 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
+
+const app = express()
+const port = 3000
+
+
 
 app.use(express.static('public'))
 app.use(express.json())
