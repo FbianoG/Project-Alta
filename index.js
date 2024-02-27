b1cdc7a987e32d7479b6a569db4924a9bc64e745
F.Gonçalves <boriscasoy123@gmail.com>
2024-02-27 10:14:11 -0300

    s

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const express = require('express')
 const DB = require('./src/dataBase/db')
 const app = express()
 
+const corsOptions = {
+    origin: 'http://127.0.0.1:5500/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    exposedHeaders: 'Authorization',
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 const router = require('./src/routes/router')
 
