ef12fe8505a3e6d038c4a008213f9e669adb832c
F.Gonçalves <boriscasoy123@gmail.com>
2024-02-28 20:45:37 -0300

    df

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -11,17 +11,12 @@ async function login(req, res) {
         if (!username || !password) {
             return res.status(400).json({ auth: false, status: 400, message: "Preencha todos os campos!" })
         }
-
         const userFind = await User.findOne({ username, password })
-
         if (!userFind) {
             return res.status(400).json({ auth: false, status: 400, message: "Login ou senha inválido!" })
         }
-
         // const token = await mid.createToken(userFind._id)
-
         return res.status(200).json({ auth: true, status: 200, message: "Logado com sucesso!", })
-
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: "Ocorreu algum erro de servidor!" })
--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,19 +1,16 @@
-
-
+// cores.js (seu middleware CORS)
 async function cores(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500'); // Substitua pelo URL real do seu frontend
     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Credentials', true); // Se você estiver usando credenciais
+    res.header('Access-Control-Allow-Credentials', true);
 
-    // Permitir que o navegador envie solicitações OPTIONS (usado para preflight)
     if (req.method === 'OPTIONS') {
         res.sendStatus(200);
     } else {
         next();
     }
-
 }
 
-
 module.exports = { cores };
+
