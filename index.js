c3499baf28ba6fc0da3e410d9274909883163183
F.Gonçalves <boriscasoy123@gmail.com>
2024-02-28 20:35:38 -0300

    s

--- index.js
+++ index.js
@@ -4,10 +4,42 @@ const DB = require('./src/dataBase/db')
 const app = express()
 
 
+
+
+
+
+
+
+
+
+
+
+
 app.use(cors());
 
 
 const router = require('./src/routes/router')
+
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', true); // Se você estiver usando credenciais
+
+    // Permitir que o navegador envie solicitações OPTIONS (usado para preflight)
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
+
+
+
+
 
 
 const port = 3000
---
+++ src/middlewares/cors.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Middleware CORS
+
+
+// Suas rotas e controladores aqui
+
+module.exports = router;
