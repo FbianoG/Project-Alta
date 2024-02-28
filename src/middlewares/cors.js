f76e4f756efc6cae94319dcdd2a1562dd108ce38
F.Gonçalves <boriscasoy123@gmail.com>
2024-02-28 20:40:21 -0300

    s

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const router = express.Router();
-
-// Middleware CORS
 
 
-// Suas rotas e controladores aqui
+async function cores(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', true); // Se você estiver usando credenciais
 
-module.exports = router;
+    // Permitir que o navegador envie solicitações OPTIONS (usado para preflight)
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+
+}
+
+
+module.exports = { cores };
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const control = require('../controllers/controller')
 const mid = require("../middlewares/jwtoken")
+const cor = require("../middlewares/cors")
 
 
 
@@ -8,7 +9,7 @@ router.get("/", async (req, res) => {
     res.json({ message: "Tudo certo" })
 })
 
-router.post("/login", control.login)
+router.post("/login", cor.cores, control.login)
 
 router.post("/getPatient", mid.verifyToken, control.getPatient)
 
