cc3bd1b33526ca590b0f3165f69e4e46dea374c4
F.Gonçalves <boriscasoy123@gmail.com>
2024-02-28 23:56:17 -0300

    sdf

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-const cors = require('cors')
 const express = require('express')
-const DB = require('./src/dataBase/db')
-const app = express()
-
-
-
-app.use(cors());
-
-
 const router = require('./src/routes/router')
+const DataBase = require('./src/dataBase/db')
 
-
-
+const cors = require('cors');
+const app = express()
 const port = 3000
 
-
-
+app.use(cors());
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
 
+DataBase.connectDataBase()
+
 app.listen(port, () => {
     console.log(`Servidor funcionando na porta:`, port)
-    DB.ConnectDataBase()
 })
